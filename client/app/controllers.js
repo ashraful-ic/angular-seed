0e65618e3016c83a03e69d3d621f213b38681c7a
Faisal Mahmud <faisal@willandskill.se>
2014-11-19 21:45:21 +0100

    Added AppCtrl

---
+++ client/app/controllers.js
@@ -0,0 +1,11 @@
+(function(){
+
+angular.
+    module('seed').Controller('AppCtrl', Ctrl);
+
+    /* @ngInject */
+    function Ctrl($http){
+        console.info('In tha controller...');
+    }
+})();
+
