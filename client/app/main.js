b17174c1a67ed56306f48932aa5add2b590bae75
Faisal Mahmud <faisal@willandskill.se>
2014-11-25 23:40:17 +0100

    Made main.js easier to read

--- client/app/main.js
+++ client/app/main.js
@@ -1,2 +1,12 @@
 // Rename module from 'seed' to something more appropriate
-angular.module('seed', []);
+angular.module('seed',
+    // Declare depencies below...
+    [
+        'ngSanitize',
+        'ngRoute',
+        'angular-lodash',
+        'ui.router',
+        'pascalprecht.translate',
+        'angularMoment',
+        'ui.bootstrap'
+    ]);
